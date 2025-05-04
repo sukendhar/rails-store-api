8e52a01624445305da687b966ff63a44a04de2bd
sukendhar <sukendhar555@gmail.com>
2025-05-04 12:02:15 -0500

    Added faker data with for test APIs

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  store = Store.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    description: Faker::Restaurant.description
+  )
+
+  rand(5..20).times do
+    item = store.items.create!(
+      name: Faker::Food.dish,
+      price: Faker::Commerce.price(range: 5..30.0),
+      description: Faker::Food.description
+    )
+
+    rand(5..10).times do
+      item.ingredients.create!(
+        name: Faker::Food.ingredient,
+        quantity: Faker::Food.measurement
+      )
+    end
+  end
+end
